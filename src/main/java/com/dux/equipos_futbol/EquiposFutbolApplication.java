5f75da66b4641f46d15a3d474f0de46b927395e1
Franco Barbieri <franco.barbieri@techforb.com>
2025-04-18 16:52:54 -0300

    ADD: Commit inicial

---
+++ src/main/java/com/dux/equipos_futbol/EquiposFutbolApplication.java
@@ -0,0 +1,13 @@
+package com.dux.equipos_futbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquiposFutbolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dux/equipos_futbol/EquiposFutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dux.equipos_futbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquiposFutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
